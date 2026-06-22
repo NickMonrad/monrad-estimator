b89e400660984f3026d88f3c26fc35a388767d3e
lokhor <lokhor@localhost>
2026-06-22 22:44:53 +1000

    fix(#271): add missing useState imports to sub-modules

--- client/src/hooks/useAllocationEditing.ts
+++ client/src/hooks/useAllocationEditing.ts
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { api } from '../lib/api'
 import { invalidateProjectResourceProfile } from '../lib/projectInvalidation'
