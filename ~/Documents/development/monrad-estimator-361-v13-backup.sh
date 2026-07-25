179aa8c79eba758a4ef2f23820b487247991a9a4
lokhor <lokhor@localhost>
2026-07-25 17:49:20 +1000

    chore(#363): remove accidental committed local agent and backup paths

--- ~/Documents/development/monrad-estimator-361-v13-backup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/lokhor/Documents/development/monrad-estimator-361-v13
-cp -r ./* ~/Documents/development/monrad-estimator-361-v13-backup/ 2>/dev/null || echo "Note: Some files may not have been backed up"
-echo "Backup created at ~/Documents/development/monrad-estimator-361-v13-backup"
